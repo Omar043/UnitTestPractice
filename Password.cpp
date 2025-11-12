3633d8649f90c97910df9e6a5c29352bbe6cc37b
Omar Rodriguez-Bernal <20omarr04@gmail.com>
2025-11-12 11:36:17 -0800

    added tests

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,7 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
+
 
 using std::string;
 
@@ -24,6 +26,17 @@ int Password::count_leading_characters(string phrase){
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+// Implementation C
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
   return false;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -19,4 +19,56 @@ TEST(PasswordTest, single_letter_password)
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
-}
+};
+
+TEST(PasswordTest, blank_char)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case(""));
+};
+
+TEST(PasswordTest, single_lower_char)
+{
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("a"));
+};
+
+TEST(PasswordTest, single_upper_char){
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("A"));
+};
+
+TEST(PasswordTest, mult_lower_char){
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("aaaa"));
+};
+
+TEST(PasswordTest, mult_upper_char){
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("AAAA"));
+};
+
+TEST(PasswordTest, num_and_char){
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("1a11aaa"));
+};
+
+TEST(PasswordTest, num_and_single_char){
+	Password my_password;
+	ASSERT_FALSE(my_password.has_mixed_case("1a"));
+};
+
+TEST(PasswordTest, single_num_single_char){
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("aA"));
+};
+
+TEST(PasswordTest, mult_upper_one_single){
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("AAAAa"));
+};
+
+TEST(PasswordTest, mult_single_one_upper){
+	Password my_password;
+	ASSERT_TRUE(my_password.has_mixed_case("aAAAA"));
+};
